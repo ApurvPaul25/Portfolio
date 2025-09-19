ffcdc9b2a857884f412786bb1b7e6158a389a0ee
Apurva Paul <47327058+ApurvPaul25@users.noreply.github.com>
2025-09-19 23:38:09 +0530

    Add files via upload

---
+++ gradient.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./gradient.css"
+
+export default function Gradient() {
+  return (
+   <div className="body">
+    <div className="container">
+    <div className="m">
+      <span>A</span>
+    </div>
+   </div>
+   </div>
+  )
+}
